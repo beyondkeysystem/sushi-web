161eca22c48b258aaef38b8602858f257cba4847
David Curras <dcurras@deviget.com>
2015-02-12 09:48:54 -0300

    Minor security changes

--- api/v1/config/config.php
+++ api/v1/config/config.php
@@ -3,10 +3,10 @@
 /**
  * Database
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'sushiweb');
+define('DB_USERNAME', 'root'); //delimall_admin
+define('DB_PASSWORD', ''); //51087luz#niversal#2088
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'sushiweb'); //delimall_prod
 
 /**
  * Session
